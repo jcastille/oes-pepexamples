e458496f4d82c9b69b1046b3e1d95d628d4c4655
Jerry Castille <jerry.castille@pathmaker-group.com>
2015-03-17 15:19:10 -0500

    This is an updated version to include the group name in the main argument

--- src/com/pathmaker/oracle/oes/examples/OESPepQuery.java
+++ src/com/pathmaker/oracle/oes/examples/OESPepQuery.java
@@ -11,6 +11,7 @@ import com.bea.security.RuntimeAction;
 
 import oracle.security.jps.openaz.pep.*;
 import weblogic.security.principal.WLSUserImpl;
+import weblogic.security.principal.WLSGroupImpl;
 
 import javax.security.auth.Subject;
 
@@ -23,8 +24,9 @@ public class OESPepQuery {
     	String RESOURCE_TYPE=null;
     	String RESOURCE_NAME=null;
     	String USER_NAME=null;
+    	String GROUP_NAME=null;
     	if (args.length < 4) {
-        	System.out.println("Usage: OESPepQuery <application_name> <resource_type> <resource_name> <user_name>");
+        	System.out.println("Usage: OESPepQuery <application_name> <resource_type> <resource_name> <user_name> <group_name>");
             System.exit(0);
 
         } else {
@@ -32,17 +34,22 @@ public class OESPepQuery {
         	RESOURCE_TYPE = args[1];
         	RESOURCE_NAME = args[2];
         	USER_NAME = args[3];
+        	if (args.length > 4){
+        		GROUP_NAME = args[4];
+        	}
         }
         OESPepQuery app = new OESPepQuery();
-        app.pepQueryExample(APPLICATION_NAME,RESOURCE_TYPE,RESOURCE_NAME,USER_NAME);
+        app.pepQueryExample(APPLICATION_NAME,RESOURCE_TYPE,RESOURCE_NAME,USER_NAME,GROUP_NAME);
         System.exit(0);
     }
     
     @SuppressWarnings("rawtypes")
-	public void pepQueryExample(String APPLICATION_NAME, String RESOURCE_TYPE, String RESOURCE_NAME, String USER_NAME){
+	public void pepQueryExample(String APPLICATION_NAME, String RESOURCE_TYPE, String RESOURCE_NAME, String USER_NAME, String GROUP_NAME){
     	Subject subject;
     	subject = new Subject();
     	subject.getPrincipals().add(new WLSUserImpl(USER_NAME));
+    	if (GROUP_NAME != null)
+    		subject.getPrincipals().add(new WLSGroupImpl(GROUP_NAME));
     	
     	PepRequestFactory pepFactory;
     	pepFactory = PepRequestFactoryImpl.getPepRequestFactory();
@@ -58,21 +65,21 @@ public class OESPepQuery {
             String scope = "resource=" + RESOURCE_STRING;
             System.out.println("PepSampleApp: query scope is: " + scope);
             System.out.println("-----------------------------------------------------------------------------------------");
-            //Invoke newPepQueryRequest passing Subject, environment, scope and PepRequestQueryType (Verbose will return both allow and deny)
+            /*Invoke newPepQueryRequest passing Subject, environment, scope and PepRequestQueryType (Verbose will return both allow and deny)*/
             PepRequest pepRequest = pepFactory.newQueryPepRequest(subject, env, scope, PepRequestQueryType.VERBOSE);
 
-            // Retrieves the PepResponse object
+            /* Retrieves the PepResponse object*/
             PepResponse pepResponse = pepRequest.decide();
             while (pepResponse.next()){;
             	System.out.println();
-            	//retrieve the list of actions from the PepResponse
+            	/*retrieve the list of actions from the PepResponse*/
             	ArrayList arrayList = (ArrayList) pepResponse.getAction();
             	List grantedActions = null;
             	List deniedActions = null;
             	if (arrayList != null) {
-            		//grantedActions is returned as the first element of the list
+            		/*grantedActions is returned as the first element of the list*/
             		grantedActions = (List) arrayList.get(0);
-            		//deniedActions is returned as the second element of the list
+            		/*deniedActions is returned as the second element of the list*/
             		deniedActions = (List) arrayList.get(1);
             	}
             	if (grantedActions != null) {
@@ -81,7 +88,7 @@ public class OESPepQuery {
             		while (iterator.hasNext()) {
             			thisAction = ((RuntimeAction) iterator.next()).getAction();
             			System.out.println(pepResponse.getResource().toString() + " granted action:  " + thisAction);
-            			// Perform a direct requests using the Decision API to get the obligations if there are any for this allowed action
+            			/* Perform a direct requests using the Decision API to get the obligations if there are any for this allowed action*/
             			PepResponse response = PepRequestFactoryImpl.getPepRequestFactory().newPepRequest(subject,thisAction,pepResponse.getResource().toString(),env).decide();
                   		Map<String, Obligation> obligations = response.getObligations();
                   		for (String name : obligations.keySet()){                
